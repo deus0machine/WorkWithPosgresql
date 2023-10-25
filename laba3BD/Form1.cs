e6afbc6e8530abb636c118b6160565e48254c747
deus0machine <aburgomeistus@yandex.ru>
2023-10-25 18:29:32 +0300

    Добавьте файлы проекта.

---
+++ laba3BD/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace laba3BD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connstring = String.Format("Server={0};Port={1};" + "User Id ={2};Password={3};Database={4}", "localhost", 5432, "postgres", "18201905", "localbd");
+        NpgsqlConnection conn;
+        string sql;
+        NpgsqlCommand cmd;
+        DataTable dt;
+        int rowIndex = -1;
+        private void button1_Click(object sender, EventArgs e)//insert
+        {
+            rowIndex = -1;
+            textBox1.Enabled = textBox2.Enabled = textBox3.Enabled = true;
+            textBox1.Text = textBox2.Text = textBox3.Text = null;
+            textBox1.Select();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
+            Select();
+        }
+        private new void Select()
+        {
+            try 
+            {
+                conn.Open();
+                sql = @"select * from st_select()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                conn.Close();
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                rowIndex = e.RowIndex;
+                textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells["firstname"].Value.ToString();
+                textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["midname"].Value.ToString();
+                textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells["lastname"].Value.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//update
+        {
+            if (rowIndex < 0)
+            {
+                MessageBox.Show("Choose student to update");
+                return;
+            }
+            textBox1.Enabled = textBox2.Enabled = textBox3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e) // delete
+        {
+            if (rowIndex < 0)
+            {
+                MessageBox.Show("Choose student to delete");
+                return;
+            }
+             
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_delete(:_id)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id", int.Parse(dataGridView1.Rows[rowIndex].Cells["id"].Value.ToString()));
+                if((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Delete student successfully");
+                    rowIndex = -1;
+                    conn.Close();
+                    Select();
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Deleted fail. Error: " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) //save
+        {
+            int result = 0;
+            if (rowIndex < 0) //insert
+            {
+                try
+                {
+                    conn.Open();
+                    sql = @"select * from st_insert(:_firstname, :_midname, :_lastname)";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("_firstname", textBox1.Text);
+                    cmd.Parameters.AddWithValue("_midname", textBox2.Text);
+                    cmd.Parameters.AddWithValue("_lastname", textBox3.Text);
+                    result = (int)cmd.ExecuteScalar();
+                    conn.Close();
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Inserted new student successfully");
+                        Select();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Inserted fail");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    conn.Close() ;
+                    MessageBox.Show("Inserted fail. Error: " + ex.Message);
+                }
+            }
+            else //update
+            {
+                try
+                {
+                    conn.Open();
+                    sql = @"select * from st_update(:_id, :_firstname, :_midname, :_lastname)";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("_id", int.Parse(dataGridView1.Rows[rowIndex].Cells["id"].Value.ToString()));
+                    cmd.Parameters.AddWithValue("_firstname", textBox1.Text);
+                    cmd.Parameters.AddWithValue("_midname", textBox2.Text);
+                    cmd.Parameters.AddWithValue("_lastname", textBox3.Text);
+                    result= (int)cmd.ExecuteScalar();
+                    conn.Close();
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Updated successfully");
+                        Select();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Updated failed");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                    MessageBox.Show("Deleted fail. Error: " + ex.Message);
+                }
+            }
+            result = 0;
+            textBox1.Text = textBox2.Text = textBox3.Text = null;
+            textBox1.Enabled = textBox2.Enabled = textBox3.Enabled = false;
+        }
+    }
+}
---
+++ laba3BD/Form2.cs
@@ -0,0 +1,63 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace laba3BD
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;database=testdb;User Id=postgres;password=18201905;");
+
+            DataSet set1 = new DataSet();
+            set1.Clear();
+            // Открываем подключение 
+            conn.Open();
+            // Sql-запрос для формирования списка 
+            NpgsqlCommand command1 = new NpgsqlCommand("select name ,id from student", conn);
+            // Адаптер 
+            NpgsqlDataAdapter da1 = new NpgsqlDataAdapter(command1);
+            // Заполнение набора данных 
+            da1.Fill(set1, "set2");
+            // Связывание списка и набора данных 
+            comboBox1.DataSource = set1.Tables["set2"];
+            // Данные, которые будут отображены в списке 
+            comboBox1.DisplayMember = "name";
+            // Значения, соотвествующие отображенным элементам списка 
+            comboBox1.ValueMember = "id";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;database=testdb;User Id=postgres;password=18201905;");
+            conn.Open();
+            NpgsqlCommand command = new NpgsqlCommand("insert into student (name,id) values(:name,:id)", conn);
+            // Данные из поля и списка добавляем в параметры запроса
+            command.Parameters.Add(new NpgsqlParameter("name", DbType.String));
+            command.Parameters[0].Value = Convert.ToString(textBox1.Text);
+            command.Parameters.Add(new NpgsqlParameter("id", DbType.Int32));
+            // Значение параметра=id, соответствующему выбранному значению из списка
+            //command.Parameters[1].Value = Convert.ToInt32(comboBox1.SelectedValue);
+            command.Parameters[1].Value = 1;
+            // Выполнить команду
+            command.ExecuteNonQuery();
+            conn.Close();
+            this.Close();
+
+        }
+    }
+}
